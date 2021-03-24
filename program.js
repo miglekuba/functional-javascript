0b1316390c1aaf22a5d6fa8bc0381e6928872f01
Migle Kubiliute <migle.kubiliute@bglgroup.co.uk>
2021-03-24 19:39:54 +0000

    Update program.js

--- program.js
+++ program.js
@@ -1,38 +1,48 @@
-// Write a function that takes an input string and returns it uppercased.
+// 1. Write a function that takes an input string and returns it uppercased.
 
 let func = (input) => input.toUpperCase();
 module.exports = func;
 
-// Higher Order Functions
+//2. Implement a function that takes a function as its first argument, a number num as its second argument, then executes the passed in function num times.
+
 let repeat = (operation, num) => {
   return operation() * num;
 };
 module.exports = repeat;
 
-// Map
+/* 3. Convert the following code from a for-loop to Array.map:
+
+    function doubleAll(numbers) {
+      var result = []
+      for (var i = 0; i < numbers.length; i++) {
+        result.push(numbers[i] * 2)
+      }
+      return result
+    }
+
+    module.exports = doubleAll
+*/
+
 function doubleAll(numbers) {
   return numbers.map((x) => x * 2);  // Also could be done in one line: const doubleAll = numbers.map(x => x * 2)
 }
 module.exports = doubleAll;
 
-/* Use Array#filter to write a function called getShortMessages.
-
+/* 4. Use Array.filter to write a function called getShortMessages.
 getShortMessages takes an array of objects with '.message' properties and returns an array of messages that are less than < 50 characters long.
-
 The function should return an array containing the messages themselves, without their containing object.
-
-## Arguments
-
-  * messages: an Array of 10 to 100 random objects that look something like this:
-
+Arguments:
+messages: an Array of 10 to 100 random objects that look something like this:
     {
-      message: 'Esse id amet quis eu esse aute officia ipsum.' // random
+      message: 'Esse id amet quis eu esse aute officia ipsum.'
     } */
+
+//solution:
 const getShortMessages = (messages) => messages
   .filter(m => m.message.length <= 50)
   .map(m => m.message);
 
 module.exports = getShortMessages;
 
-//Every Some
+//5. Every Some
 
